fc6e0cb52b9dcd4adee120e9f0bff8ab899a298c
domHenin <domhenin.517@gmail.com>
2020-03-10 16:24:51 -0400

    still working on code

---
+++ ec2training/main.tf
@@ -0,0 +1,60 @@
+# AWS Provider
+provider "aws" {
+    version = ">= 2.46.0"
+    region = var.aws_region
+}
+
+# S3 Backend
+terraform {
+    backend "s3" {
+        bucket = "cloudlogixtrtfstate"
+        key = "ec2training/terraform.tfstate"
+        region = "us-east-2"
+    }
+}
+
+# Key Pair
+resource "aws_key_pair" "deployer" {
+    key_name = "provision_key"
+    public_key = file("~/.ssh/id_rsa.pub")
+}
+
+# EC2 AMI
+data "aws_ami" "deb_ami" {
+    owner = "var.deb_owner"
+    most_recent = true
+
+    filter {
+        name = "debian"
+        value = ["debian-stretch-hvm-*"]
+    }
+}
+
+# EC2 Instance
+resource "aws_instance" "deb_container" {
+    key_name = aws_key_pair.deployer.key_name
+    count = 2
+    ami = data.aws_ami.deb_ami.id
+    instance_type = "t2.micro"
+
+    connection {
+        type = 
+        user = 
+        private_key = file("~/.ssh/id_rsa")
+        host = self.publics_ip
+    }
+    
+    user_data = file("files/instal_nginx.sh")
+}
+
+# Create ELB
+resource "aws_elb" "container_elb" {
+    name = "container_elb"
+
+    listener {
+        instance_port = var.instance_port
+        instance_protocol = var.instance_protocol
+        lb_port = var.lb_port
+        lb_protocol = var.lb_protocol
+    }
+}
