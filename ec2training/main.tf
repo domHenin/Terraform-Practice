fe8ebf3284a556cc06d49135f98d5444f0c70311
domHenin <domhenin.517@gmail.com>
2020-03-11 13:19:33 -0400

    functioning code commit, all desired resources are being created

--- ec2training/main.tf
+++ ec2training/main.tf
@@ -1,60 +1,67 @@
 # AWS Provider
 provider "aws" {
-    version = ">= 2.46.0"
-    region = var.aws_region
+  version = ">= 2.46.0"
+  region  = var.aws_region
 }
 
 # S3 Backend
 terraform {
-    backend "s3" {
-        bucket = "cloudlogixtrtfstate"
-        key = "ec2training/terraform.tfstate"
-        region = "us-east-2"
-    }
+  backend "s3" {
+    bucket = "cloudlogixtrtfstate"
+    key    = "ec2training/terraform.tfstate"
+    region = "us-east-2"
+  }
 }
 
 # Key Pair
 resource "aws_key_pair" "deployer" {
-    key_name = "provision_key"
-    public_key = file("~/.ssh/id_rsa.pub")
+  key_name   = "provision_key"
+  public_key = file("~/.ssh/id_rsa.pub")
 }
 
 # EC2 AMI
 data "aws_ami" "deb_ami" {
-    owner = "var.deb_owner"
-    most_recent = true
+  owners      = ["379101102735"]
+  most_recent = true
 
-    filter {
-        name = "debian"
-        value = ["debian-stretch-hvm-*"]
-    }
+  filter {
+    name   = "name"
+    values = ["debian-stretch-hvm-*"]
+  }
 }
 
 # EC2 Instance
 resource "aws_instance" "deb_container" {
-    key_name = aws_key_pair.deployer.key_name
-    count = 2
-    ami = data.aws_ami.deb_ami.id
-    instance_type = "t2.micro"
+  key_name      = aws_key_pair.deployer.key_name
+  count         = 2
+  ami           = data.aws_ami.deb_ami.id
+  instance_type = "t2.micro"
 
-    connection {
-        type = 
-        user = 
-        private_key = file("~/.ssh/id_rsa")
-        host = self.publics_ip
-    }
-    
-    user_data = file("files/instal_nginx.sh")
+  connection {
+    type        = "ssh"
+    user        = "admin"
+    private_key = file("~/.ssh/id_rsa")
+    host        = self.publics_ip
+  }
+
+  user_data = file("files/install_nginx.sh")
 }
 
 # Create ELB
 resource "aws_elb" "container_elb" {
-    name = "container_elb"
+  name = "container-elb"
 
-    listener {
-        instance_port = var.instance_port
-        instance_protocol = var.instance_protocol
-        lb_port = var.lb_port
-        lb_protocol = var.lb_protocol
-    }
+
+  listener {
+    instance_port     = var.instance_port
+    instance_protocol = var.instance_protocol
+    lb_port           = var.lb_port
+    lb_protocol       = var.lb_protocol
+  }
+}
+
+# Create ELB Attachment
+resource "aws_elb_attachment" "elb_attachment" {
+  elb      = aws_elb.container_elb.id
+  instance = "aws_instance.deb_container.*.id"
 }
--- ec2training/variables.tf
+++ ec2training/variables.tf
@@ -1,20 +1,42 @@
 # Region Variable
 variable "aws_region" {
-    description = "regional variable"
-    default = "us-east-2"
+  description = "regional variable"
+  default     = "us-east-2"
 }
 
 # Debian AMI Owner Variable
 variable "deb_owner" {
-    description = "owner of the debian ami"
-    default = "379101102735"
+  type        = string
+  description = "owner of the debian ami"
+  default     = "379101102735"
 }
 
 # Instance Port Variable
 variable "instance_port" {
-    description = "instnace port variable"
-    default = 8000
+  description = "instnace port variable"
+  default     = 8000
 }
 
 # Instance Protocol Variable
-variable "instance_protocol
+variable "instance_protocol" {
+  description = "instance portocol variable"
+  default     = "http"
+}
+
+# Elastic Load Balancing Name Variable
+variable "container_name" {
+  description = "Elastic Load Balancing Naming Variable"
+  default     = "container-elb"
+}
+
+# Load Balance Port Variable
+variable "lb_port" {
+  description = "load balance port variable"
+  default     = "443"
+}
+
+# Load Balance Protocol Variable
+variable "lb_protocol" {
+  description = "load balance protocol variable"
+  default     = "https"
+}
