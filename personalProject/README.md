7605976bc23fbeff800bab6ff43f4387aa8cf6ba
domHenin <domhenin.517@gmail.com>
2020-09-10 18:05:46 -0400

    worked on a personal project. just so i can get use to the language again.

---
+++ personalProject/README.md
@@ -0,0 +1,10 @@
+# TODO:
+# 1 create VPC
+# 2 create internet gateway
+# 3 create custom route table
+# 4 creat a subnet
+# 5 associate subnet with route table create security group to allow port 22,80,443
+# 6 create security group to allow 22,80,443
+# 7 create a network interface with an ip in the subnet that was create in step 4
+# 8 assign an elastic IP to the network interface created in step 7
+# 9 create Ubuntu server and install/enable apache2
---
+++ personalProject/variable.tf
@@ -0,0 +1,45 @@
+# AWS Region Variable
+variable "aws_region" {
+    description = "AWS Region Variable"
+    default = "us-east-2"
+}
+
+# TLS Port Variable
+variable "tls_port" {
+    description = "port for TLS 443"
+    default = "443"
+}
+
+# SSH Port Variable
+variable "ssh_port" {
+    description = "port for SSH 22"
+    default = "22"
+}
+
+# HTTP Port Variable
+variable "http_port" {
+    description = "port for HTTP 80"
+    default = "80"
+}# AWS Region Variable
+variable "aws_region" {
+    description = "AWS Region Variable"
+    default = "us-east-2"
+}
+
+# TLS Port Variable
+variable "tls_port" {
+    description = "port for TLS 443"
+    default = "443"
+}
+
+# SSH Port Variable
+variable "ssh_port" {
+    description = "port for SSH 22"
+    default = "22"
+}
+
+# HTTP Port Variable
+variable "http_port" {
+    description = "port for HTTP 80"
+    default = "80"
+}
